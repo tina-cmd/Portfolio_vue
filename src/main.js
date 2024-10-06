3d55fd9959e0cfa894716f42126e72a64f7748e1
dextarfinity <xdfeverharsh@gmail.com>
2024-10-07 00:46:57 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import './index.css'
+import 'typeface-poppins';
+import 'boxicons/css/boxicons.min.css';
+
+createApp(App).mount('#app')
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'], // replace 'purge' with 'content'
+  darkMode: 'media', // you can remove this line entirely if you're not using dark mode
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
